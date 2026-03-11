a9cad82e2f406e17522c8b9be219b22d3b3e2dd6
Pol Casiñol <casiexpo10@gmail.com>
2026-03-11 15:44:52 +0100

    Tortuga

--- Tortugues.cs
+++ Tortugues.cs
@@ -1,6 +1,35 @@
 namespace Peixera_Virtual;
 
-public class Tortugues
+public class Tortuga: Animals_peixera
 {
-    
+    Random R = new Random();
+    public Tortuga(int x, int y, Sexes h) : base(x, y, h)
+    {
+    }
+
+    public override Animals_peixera? HeTrobatUnAltre(Animals_peixera altre)
+    {
+        if (altre is Tortuga)
+        {
+            if (Sexe == altre.Sexe)
+            {
+                Mata();
+                altre.Mata();
+            }
+            else
+            {
+                int Sexe_Nova_Tortuga = R.Next(1, 2);
+
+                if (Sexe_Nova_Tortuga == 1)
+                {
+                    return new Peix(X, Y, Sexes.Masculi);
+                }
+                else
+                {
+                    return new Peix(X, Y, Sexes.Femeni);
+                }
+            }
+        }
+        return null;
+    }
 }
